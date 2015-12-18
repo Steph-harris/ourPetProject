8e116fcf080a69df6f422223e0760b551833f11e
steph-harris <steph.harris21@gmail.com>
2015-12-17 20:41:42 -0500

    added button to html to test wiki api call

--- scripts/wikiCall.js
+++ scripts/wikiCall.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-  //code for making wiki call for animal breed
+  //code for making wiki call for animal breed (need to get breed from specific pet petfinder object)
+  $(".breed").on("click", function() {
+    alert("he");
+  });
+  // $.ajax({
+  //   type:"GET",
+  //   url: 
+  //   success: {}
+  //   error: 
+  // });
 });
