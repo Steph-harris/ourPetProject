ae9821666e5d2f67a259859e4fb2d797326df41b
thenriquez <thenriquez25@outlook.com>
2015-12-19 10:45:56 -0500

    Uhh I hate google api

--- js/Apis.js
+++ js/Apis.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
 
-${ alert.('heyyy')
+$(document).on("click", ".btn waves-effect waves-light", function() {
+	
+});
+$ function initAutocomplete()	{
+	var map = new google.maps.Maps(document)
+	center: {lat:-33.8688, lng: 151.2195},
+	zoom: 13,
+	mapTypeId: google.maps.mapTypeId.
+});
 
-})
+
