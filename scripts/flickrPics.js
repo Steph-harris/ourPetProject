d480375648ab3527ea94d19608d2c9f7792e40b9
steph-harris <steph.harris21@gmail.com>
2015-12-21 16:01:25 -0500

    added some carousel components for build and class active to first slide

--- scripts/flickrPics.js
+++ scripts/flickrPics.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   var flickrApiUrl = "https://crossorigin.me/https://api.flickr.com/services/rest/?";
   var flickrApiParams = {
     api_key: "53f3ad616d891ed27a09c64d67ff8ec5",
-    text: "dog",
+    text: $("breedCheck").val() || "dog",
     per_page: 5,
     method: "flickr.photos.search",
     format: "json",
@@ -14,9 +14,12 @@ $(document).ready(function(){
     url: flickrApiUrl + $.param(flickrApiParams),
     success: function(response){
       var flickrPetPics = response.photos.photo
-      for(i=0; i<flickrPetPics.length; i++)
-      {
-        makeFlickrCarousel(flickrPetPics[i]);
+      for(i=0; i<flickrPetPics.length; i++){
+        var newSlide = makeFlickrCarousel(flickrPetPics[i]);
+               
+        $('.carousel-item').first().addClass('active');
+        $('#flickrPic').carousel();
+        $("#flickrPic").append(newSlide);
       }
     }
   });
@@ -24,10 +27,14 @@ $(document).ready(function(){
   function makeFlickrCarousel(photoInfo){
     var flickPicUrl = "https://farm" + photoInfo.farm + ".staticflickr.com/"
     flickPicUrl += photoInfo.server + "/" + photoInfo.id + "_" + photoInfo.secret
-    flickPicUrl += "_m.jpg";
+    flickPicUrl += "_q.jpg";
 
     //Build carousel pieces
-    var newDiv = $("<div>").addClass("carousel-slider")
+    var newItem = $("<a>").addClass("carousel-item")
+    var flickrImg = $("<img>").attr("src", flickPicUrl)
+
+    newItem.append(flickrImg)
+
+    return newItem;
   }
-  $.ajax();
 });
