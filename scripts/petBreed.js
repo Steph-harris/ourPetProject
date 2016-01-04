20d0594c93e44534967d5fcd52f34ab8b957ce54
steph-harris <steph.harris21@gmail.com>
2016-01-04 15:41:44 -0500

    added new row for petfinder photos, added margin separation for small wells

--- scripts/petBreed.js
+++ scripts/petBreed.js
@@ -23,6 +23,8 @@ $(document).ready(function(){
 
   $(".breedSearch").on("click", function(e) {
     e.preventDefault();
+    debugger;
+    //$(".newPet").empty();
     searchByBreed();
   });
 
@@ -69,6 +71,7 @@ $(document).ready(function(){
     }
 
     $("#breedResult").empty();
+    $(".photoRow").empty();
 
     $.ajax({     
       type:"GET",
@@ -92,7 +95,7 @@ $(document).ready(function(){
             var newPetPicDiv = $("<div>").addClass("col-xs-6 col-md-3");
 
             newPetPicDiv.append(newPetPic);
-            $("#breedResult").append(newPetPicDiv);
+            $(".photoRow").append(newPetPicDiv);
           }
         }
 
