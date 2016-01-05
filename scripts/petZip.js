d8f6722f7376c9b88749aff503761f76bd46ff8d
steph-harris <steph.harris21@gmail.com>
2016-01-05 18:02:42 -0500

    added zip search function

--- scripts/petZip.js
+++ scripts/petZip.js
@@ -1,26 +1,27 @@
 $(document).ready(function(){
-  $("#noAnimal").hide();
-  $("#noZip").hide();
-  $('.newSelect').select2();
-
-  $("#selectBreed").on("click", function(){
+  $(".zip").on("click", function(e){
     debugger;
-    var animal = $("#animal2").val();
+    e.preventDefault();
+    var animal = $("#animal").val();
+    var zipCode = $("#enterZip").val();
 
-    if(animal === null){
-      $("#noAnimal").show().fadeOut(3500);
+    if(zipCode === ""){
+      $("#noZip").show().fadeOut(3500);
       return;
-    } 
+    } else {
+      searchByZip();
+    }
   })
   //on animal2 selection populate the breed list
   //animal alert needs 2 event listeners: one for search button, and one for breed button
-  $("#animal2").change(function(){
-    var animal = $("#animal2").val(); 
+  // $("#animal").change(function(){
+  //   var animal = $("#animal2").val(); 
 
-    $(".menu").empty();  
+  //   $(".menu").empty();  
     
-    breedChecker();   
-  });
+  //   breedChecker();   
+  //   //$('#selectBreed').select2();
+  // });
 
   $(".breedSearch").on("click", function(e) {
     e.preventDefault();
@@ -28,44 +29,15 @@ $(document).ready(function(){
     searchByBreed();
   });
 
-  function breedChecker(){   
-    var animal = $("#animal2").val();
-    var petFAPI = "https://crossorigin.me/https://api.petfinder.com/breed.list?"
+  function searchByZip(){   
+    var animal = $("#animal").val();
+    var zipVal = $("#enterZip").val();
+    var petFAPI = "https://crossorigin.me/https://api.petfinder.com/pet.find?"
     var petFAPIParam = {
       key: "311acd0ca6ee16428a93eb5dafe77634",
       animal: animal,
-      format: "json"
-    }
-
-    $.ajax({     
-      type:"GET",
-      url: petFAPI + $.param(petFAPIParam),
-      success: function(response){
-        var breeds = response.petfinder.breeds.breed;
-        for(i=0; i<breeds.length; i++){
-          var lstItm = breeds[i]['$t'];
-          var newDiv = $("<option>").attr("value",[lstItm]).text(lstItm);
-        $(".menu").append(newDiv);
-          //add class item to each and append to .menu
-          function newList(lstItm){;
-            var newDiv =("<div>").addClass("item")
-              .text(lstItm);
-            $(".menu").append(newDiv);       
-          }
-        }
-        return newDiv;
-      }
-    })
-  };
-
-  function searchByBreed(){   
-    var animal = $("#animal2").val();
-    var breedVal = $("#selectBreed").val();
-    var petFAPI = "https://crossorigin.me/https://api.petfinder.com/pet.getRandom?"
-    var petFAPIParam = {
-      key: "311acd0ca6ee16428a93eb5dafe77634",
-      animal: animal,
-      breed: breedVal,
+      location: zipVal,
+      count: 1,
       output: "full",
       format: "json"
     }
@@ -77,10 +49,10 @@ $(document).ready(function(){
       type:"GET",
       url: petFAPI + $.param(petFAPIParam),
       success: function(response){
-        var newPetInfo = response.petfinder.pet
-        var newPetContact = response.petfinder.pet.contact
-        var petOptions = response.petfinder.pet.options.option;
-        var petPhoto = response.petfinder.pet.media.photos.photo;
+        var newPetInfo = response.petfinder.pets.pet
+        var newPetContact = response.petfinder.pets.pet.contact
+        var petOptions = response.petfinder.pets.pet.options.option;
+        var petPhoto = response.petfinder.pets.pet.media.photos.photo;
         var yourPet = $("<h2>Meet " + newPetInfo.name["$t"] + ", a size " + newPetInfo.size["$t"]+" " + newPetInfo.age["$t"] +" "+ newPetInfo.sex["$t"] + " from " + newPetContact.city["$t"] +", "+newPetContact.state["$t"] + "</h2>");
         var yourPetP = $("<p>").addClass("col-xs-12 col-md-10 col-md-offset-1").text(newPetInfo.description["$t"]);
         var yourPetContact = $("<h3>To adopt " + newPetInfo.name["$t"] + ", please call " + newPetContact.phone["$t"] +"</h3>").addClass("col-xs-12");
@@ -123,4 +95,3 @@ $(document).ready(function(){
   };
 });
 
-    
